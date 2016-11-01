c5bd99812eb197279f57e546f3f71f3e410da649
R-ToS <asaka666@gmail.com>
2016-11-02 06:53:35 +0900

    local function add

--- addon_d.ipf/tkb/tkb.lua
+++ addon_d.ipf/tkb/tkb.lua
@@ -1,16 +1,16 @@
 -----------------------------------
 -- 特性[ヒールダメージ除去]と[プロヴォック]を
 -- ボタンでON/OFF出来るようにしたアドオン
--- 2016.10.26
+-- 2016.11.02
 -----------------------------------
-local load = false;
+local load = false
 function TKB_ON_INIT(addon, frame)
 	local a = require('acutil')
 	a.slashCommand('/tkb', TKB)
 	a.slashCommand('/tkbfn', TKBfn)
 	addon:RegisterMsg('GAME_START', 'TKB')
 	if not load then
-		load = true;
+		load = true
 		CHAT_SYSTEM('TKB Loaded')
 	end
 end
@@ -28,23 +28,23 @@ function TKB(frame, control)
 	btn:Resize(80, 40)
 	btn:SetOffset(0, 0)
 	btn:SetEventScript(ui.LBUTTONUP, 'TKBfn')
+
+	local function TKBtogbtn(Cabi)
+		frame:ShowWindow(1)
+		local state = GetIES(Cabi:GetObject()).ActiveState
+		if state == 0 then
+			btn:SetText('OFF')
+		elseif state == 1 then
+			btn:SetText('ON')
+		end
+	end
+
 	local cl = session.GetAbility(401016)
 	local sw = session.GetAbility(101020)
-	if cl then frame:ShowWindow(1)
-		local state = GetIES(cl:GetObject()).ActiveState;
-		if state == 0 then
-			btn:SetText('OFF')
-		elseif state == 1 then
-			btn:SetText('ON')
-		end
+	if cl then
+		TKBtogbtn(cl)
 	elseif sw then
-		frame:ShowWindow(1)
-		local state = GetIES(sw:GetObject()).ActiveState;
-		if state == 0 then
-			btn:SetText('OFF')
-		elseif state == 1 then
-			btn:SetText('ON')
-		end
+		TKBtogbtn(sw)
 	else
 		frame:ShowWindow(0)
 	end
@@ -58,33 +58,27 @@ function TKBfn(frame, control)
 	local btn = frame:GetChild('btn')
 	local sframe = ui.GetFrame('skilltree')
 	sframe:SetUserValue('CLICK_ABIL_ACTIVE_TIME', imcTime.GetAppTime()-10)
+
+	local function TKBtogfn(Cabi,Cname,Cid)
+		local name = Cname
+		local id = Cid
+		local abi = GetIES(Cabi:GetObject())
+		local state = abi.ActiveState
+		if state == 0 then
+			CHAT_SYSTEM('[ON] '..abi.Name)
+			btn:SetText('ON')
+		elseif state == 1 then
+			CHAT_SYSTEM('[OFF] '..abi.Name)
+			btn:SetText('OFF')
+		end
+		TOGGLE_ABILITY_ACTIVE(nil, nil, name, id)
+	end
+
 	local cl = session.GetAbility(401016)
 	local sw = session.GetAbility(101020)
 	if cl then
-		local name = 'Cleric10'
-		local id = 401016;
-		local abil = GetIES(cl:GetObject())
-		local state = abil.ActiveState;
-		if state == 0 then
-			CHAT_SYSTEM('[ON] '..abil.Name)
-			btn:SetText('ON')
-		elseif state == 1 then
-			CHAT_SYSTEM('[OFF] '..abil.Name)
-			btn:SetText('OFF')
-		end
-		TOGGLE_ABILITY_ACTIVE(nil, nil, name, id)
+		TKBtogfn(cl, 'Cleric10', 401016)
 	elseif sw then
-		local name = 'Swordman28'
-		local id = 101020;
-		local abil = GetIES(sw:GetObject())
-		local state = abil.ActiveState;
-		if state == 0 then
-			CHAT_SYSTEM('[ON] '..abil.Name)
-			btn:SetText('ON')
-		elseif state == 1 then
-			CHAT_SYSTEM('[OFF] '..abil.Name)
-			btn:SetText('OFF')
-		end
-		TOGGLE_ABILITY_ACTIVE(nil, nil, name, id)
+		TKBtogfn(sw, 'Swordman28', 101020)
 	end
 end
